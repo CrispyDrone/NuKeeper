3a04ce654ab765e3853d446c9b0d1c9893be8be2
CrispyDrone <mybluewaterfall@hotmail.com>
2021-02-07 15:37:45 +0100

    temp

--- NuKeeper/Commands/UpdateCommand.cs
+++ NuKeeper/Commands/UpdateCommand.cs
@@ -1,56 +1,85 @@
-using System.Threading.Tasks;
 using McMaster.Extensions.CommandLineUtils;
-using NuKeeper.Abstractions;
 using NuKeeper.Abstractions.Configuration;
-using NuKeeper.Inspection.Logging;
-using NuKeeper.Local;
+using NuKeeper.Abstractions.Logging;
+using NuKeeper.Abstractions.NuGet;
+using NuKeeper.Abstractions.Output;
 
 namespace NuKeeper.Commands
 {
     [Command("update", Description = "Applies relevant updates to a local project.")]
-    internal class UpdateCommand : LocalNuKeeperCommand
+    sealed internal class UpdateCommand
     {
+        [Argument(0, Description = "The path to a .sln or project file, or to a directory containing a .NET solution/project. " +
+               "If none is specified, the current directory will be used.")]
+        // ReSharper disable once UnassignedGetOnlyAutoProperty
+        // ReSharper disable once MemberCanBePrivate.Global
+        public string Path { get; }
+
         [Option(CommandOptionType.SingleValue, ShortName = "m", LongName = "maxpackageupdates",
             Description = "Maximum number of package updates to make. Defaults to 1.")]
         public int? MaxPackageUpdates { get; set; }
 
-        private readonly ILocalEngine _engine;
+        [Option(CommandOptionType.SingleValue, ShortName = "c", LongName = "change",
+            Description = "Allowed version change: Patch, Minor, Major. Defaults to Major.")]
+        public VersionChange? AllowedChange { get; set; }
 
-        public UpdateCommand(ILocalEngine engine, IConfigureLogger logger, IFileSettingsCache fileSettingsCache)
-            : base(logger, fileSettingsCache)
-        {
-            _engine = engine;
-        }
+        [Option(CommandOptionType.SingleValue, ShortName = "", LongName = "useprerelease",
+            Description = "Allowed prerelease: Always, Never, FromPrerelease. Defaults to FromPrerelease.")]
+        public UsePrerelease? UsePrerelease { get; set; }
 
-        protected override async Task<ValidationResult> PopulateSettings(SettingsContainer settings)
-        {
-            var baseResult = await base.PopulateSettings(settings);
-            if (!baseResult.IsSuccess)
-            {
-                return baseResult;
-            }
+        [Option(CommandOptionType.MultipleValue, ShortName = "s", LongName = "source",
+            Description =
+                "Specifies a NuGet package source to use during the operation. This setting overrides all of the sources specified in the NuGet.config files. Multiple sources can be provided by specifying this option multiple times.")]
+        // ReSharper disable once UnassignedGetOnlyAutoProperty
+        // ReSharper disable once MemberCanBePrivate.Global
+        public string[] Source { get; }
 
-            const int defaultMaxPackageUpdates = 1;
-            var fileSettings = FileSettingsCache.GetSettings();
+        public NuGetSources NuGetSources => Source == null ? null : new NuGetSources(Source);
 
-            var maxUpdates = Concat.FirstValue(
-                MaxPackageUpdates,
-                fileSettings.MaxPackageUpdates,
-                defaultMaxPackageUpdates);
+        [Option(CommandOptionType.SingleValue, ShortName = "a", LongName = "age",
+            Description = "Exclude updates that do not meet a minimum age, in order to not consume packages immediately after they are released. Examples: 0 = zero, 12h = 12 hours, 3d = 3 days, 2w = two weeks. The default is 7 days.")]
+        // ReSharper disable once UnassignedGetOnlyAutoProperty
+        // ReSharper disable once MemberCanBePrivate.Global
+        public string MinimumPackageAge { get; }
 
-            if (maxUpdates < 1)
-            {
-                return ValidationResult.Failure($"Max package updates of {maxUpdates} is not valid");
-            }
+        [Option(CommandOptionType.SingleValue, ShortName = "i", LongName = "include",
+            Description = "Only consider packages matching this regex pattern.")]
+        public string Include { get; set; }
 
-            settings.PackageFilters.MaxPackageUpdates = maxUpdates;
-            return ValidationResult.Success;
-        }
+        [Option(CommandOptionType.SingleValue, ShortName = "e", LongName = "exclude",
+            Description = "Do not consider packages matching this regex pattern.")]
+        public string Exclude { get; set; }
 
-        protected override async Task<int> Run(SettingsContainer settings)
-        {
-            await _engine.Run(settings, true);
-            return 0;
-        }
+        [Option(CommandOptionType.SingleValue, ShortName = "v", LongName = "verbosity",
+            Description = "Sets the verbosity level of the command. Allowed values are q[uiet], m[inimal], n[ormal], d[etailed].")]
+        public LogLevel? Verbosity { get; set; }
+
+        [Option(CommandOptionType.SingleValue, ShortName = "", LongName = "logdestination",
+            Description = "Destination for logging.")]
+        public LogDestination? LogDestination { get; set; }
+
+        [Option(CommandOptionType.SingleValue, ShortName = "", LongName = "logfile",
+            Description = "Log to the named file.")]
+        public string LogFile { get; set; }
+
+        [Option(CommandOptionType.SingleValue, ShortName = "", LongName = "outputformat",
+            Description = "Format for output.")]
+        public OutputFormat? OutputFormat { get; set; }
+
+        [Option(CommandOptionType.SingleValue, ShortName = "", LongName = "outputdestination",
+            Description = "Destination for output.")]
+        public OutputDestination? OutputDestination { get; set; }
+
+        [Option(CommandOptionType.SingleValue, ShortName = "", LongName = "outputfile",
+            Description = "File name for output.")]
+        public string OutputFileName { get; set; }
+
+        [Option(CommandOptionType.SingleValue, ShortName = "", LongName = "branchnametemplate",
+            Description = "Template used for creating the branch name.")]
+        public string BranchNameTemplate { get; set; }
+
+        [Option(CommandOptionType.SingleValue, ShortName = "git", LongName = "gitclipath",
+            Description = "Path to git to use instead of lib2gitsharp implementation")]
+        public string GitCliPath { get; set; }
     }
 }
