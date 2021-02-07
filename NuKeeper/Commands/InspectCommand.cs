828bddfd456d201fd49c1488153b83d9a5c4a154
CrispyDrone <mybluewaterfall@hotmail.com>
2021-02-07 15:51:19 +0100

    move props to InspectCommand, remove LocalNuKeeperCommand

--- NuKeeper/Commands/InspectCommand.cs
+++ NuKeeper/Commands/InspectCommand.cs
@@ -1,37 +1,81 @@
-using System.Threading.Tasks;
 using McMaster.Extensions.CommandLineUtils;
 using NuKeeper.Abstractions.Configuration;
-using NuKeeper.Inspection.Logging;
-using NuKeeper.Local;
+using NuKeeper.Abstractions.Logging;
+using NuKeeper.Abstractions.NuGet;
+using NuKeeper.Abstractions.Output;
 
 namespace NuKeeper.Commands
 {
     [Command("inspect", "i", Description = "Checks projects existing locally for possible updates.")]
-    internal class InspectCommand : LocalNuKeeperCommand
+    sealed internal class InspectCommand
     {
-        private readonly ILocalEngine _engine;
+        [Option(CommandOptionType.SingleValue, ShortName = "c", LongName = "change",
+            Description = "Allowed version change: Patch, Minor, Major. Defaults to Major.")]
+        public VersionChange? AllowedChange { get; set; }
 
-        public InspectCommand(ILocalEngine engine, IConfigureLogger logger, IFileSettingsCache fileSettingsCache) :
-            base(logger, fileSettingsCache)
-        {
-            _engine = engine;
-        }
+        [Option(CommandOptionType.SingleValue, ShortName = "", LongName = "useprerelease",
+            Description = "Allowed prerelease: Always, Never, FromPrerelease. Defaults to FromPrerelease.")]
+        public UsePrerelease? UsePrerelease { get; set; }
 
-        protected override async Task<ValidationResult> PopulateSettings(SettingsContainer settings)
-        {
-            var baseResult = await base.PopulateSettings(settings);
-            if (!baseResult.IsSuccess)
-            {
-                return baseResult;
-            }
+        [Option(CommandOptionType.MultipleValue, ShortName = "s", LongName = "source",
+            Description =
+                "Specifies a NuGet package source to use during the operation. This setting overrides all of the sources specified in the NuGet.config files. Multiple sources can be provided by specifying this option multiple times.")]
+        // ReSharper disable once UnassignedGetOnlyAutoProperty
+        // ReSharper disable once MemberCanBePrivate.Global
+        public string[] Source { get; }
 
-            return ValidationResult.Success;
-        }
+        public NuGetSources NuGetSources => Source == null ? null : new NuGetSources(Source);
 
-        protected override async Task<int> Run(SettingsContainer settings)
-        {
-            await _engine.Run(settings, false);
-            return 0;
-        }
+        [Option(CommandOptionType.SingleValue, ShortName = "a", LongName = "age",
+            Description = "Exclude updates that do not meet a minimum age, in order to not consume packages immediately after they are released. Examples: 0 = zero, 12h = 12 hours, 3d = 3 days, 2w = two weeks. The default is 7 days.")]
+        // ReSharper disable once UnassignedGetOnlyAutoProperty
+        // ReSharper disable once MemberCanBePrivate.Global
+        public string MinimumPackageAge { get; }
+
+        [Option(CommandOptionType.SingleValue, ShortName = "i", LongName = "include",
+            Description = "Only consider packages matching this regex pattern.")]
+        public string Include { get; set; }
+
+        [Option(CommandOptionType.SingleValue, ShortName = "e", LongName = "exclude",
+            Description = "Do not consider packages matching this regex pattern.")]
+        public string Exclude { get; set; }
+
+        [Option(CommandOptionType.SingleValue, ShortName = "v", LongName = "verbosity",
+            Description = "Sets the verbosity level of the command. Allowed values are q[uiet], m[inimal], n[ormal], d[etailed].")]
+        public LogLevel? Verbosity { get; set; }
+
+        [Option(CommandOptionType.SingleValue, ShortName = "", LongName = "logdestination",
+            Description = "Destination for logging.")]
+        public LogDestination? LogDestination { get; set; }
+
+        [Option(CommandOptionType.SingleValue, ShortName = "", LongName = "logfile",
+            Description = "Log to the named file.")]
+        public string LogFile { get; set; }
+
+        [Option(CommandOptionType.SingleValue, ShortName = "", LongName = "outputformat",
+            Description = "Format for output.")]
+        public OutputFormat? OutputFormat { get; set; }
+
+        [Option(CommandOptionType.SingleValue, ShortName = "", LongName = "outputdestination",
+            Description = "Destination for output.")]
+        public OutputDestination? OutputDestination { get; set; }
+
+        [Option(CommandOptionType.SingleValue, ShortName = "", LongName = "outputfile",
+            Description = "File name for output.")]
+        public string OutputFileName { get; set; }
+
+        [Option(CommandOptionType.SingleValue, ShortName = "", LongName = "branchnametemplate",
+            Description = "Template used for creating the branch name.")]
+        public string BranchNameTemplate { get; set; }
+
+        [Option(CommandOptionType.SingleValue, ShortName = "git", LongName = "gitclipath",
+            Description = "Path to git to use instead of lib2gitsharp implementation")]
+        public string GitCliPath { get; set; }
+
+        [Argument(0, Description = "The path to a .sln or project file, or to a directory containing a .NET solution/project. " +
+               "If none is specified, the current directory will be used.")]
+        // ReSharper disable once UnassignedGetOnlyAutoProperty
+        // ReSharper disable once MemberCanBePrivate.Global
+        public string Path { get; }
     }
 }
